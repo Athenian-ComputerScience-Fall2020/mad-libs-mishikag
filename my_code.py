1c7f87ad6d4182acb8731b91bf1707f8ec141bea
mishikag <23mgupta@athenian.org>
2020-09-11 17:49:17 +0000

    finished mad libs

--- my_code.py
+++ my_code.py
@@ -1,3 +1,24 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
+#  none
 
+# PERSON and I are not FEELING. We are dressing up as NOUN PLURAL and visiting the house. 
+# Before we go, we have to make sure to grab our NOUN to hold our FOOD. Happy holidays everyone from PERSON. 
+
+holiday = input("enter a holiday: ")
+insect = input("enter a insect: ")
+action = input("enter a past tense action: ")
+adjective = input("enter an adjective: ")
+person_one = input("enter the name of a person: ")
+person_two = input("enter the name of a person: ")
+person_three = input("enter the name of a person: ")
+feeling = input("enter a feeling: ")
+nounPlural = input("enter a plural noun: ")
+noun = input("enter a noun: ")
+food = input("enter a food: ")
+person_four = input("enter the name of a person: ")
+
+print("wow its already " + holiday + "!")
+print("I decorated the house with big " + insect + " webs and " + action + " pumpkins")
+print("The house looks so good! the house down my street is so " + adjective + " Mom told us to watch out because that’s where " + person_one + " and " + person_two + " live.")
+print(person_three + " and I are not " + feeling + ". We are dressing up as " + nounPlural + " and visiting the house.")
+print("Before we go, we have to make sure to grab our " + noun + "to hold our " + food + ". Happy holidays everyone from " + person_four + ".")
